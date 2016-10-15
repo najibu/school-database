e46efc182b37aab3c7859d07c892f99a5f4b2bce
najibu <ns.najibu@gmail.com>
2016-10-15 15:04:50 +0100

    Added school controller and client model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015121659) do
+ActiveRecord::Schema.define(version: 20161015140127) do
+
+  create_table "clients", force: :cascade do |t|
+    t.integer  "school_id"
+    t.integer  "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
