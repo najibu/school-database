911234bd869c73ed4c2316de73ce170c8a27cb5d
najibu <ns.najibu@gmail.com>
2016-10-15 14:52:19 +0100

    Adding a new student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,18 @@ class StudentsController < ApplicationController
 	def new
 		@student = Student.new
 	end
+
+	def create
+		@student = Student.new(student_params)
+		if @student.save 
+			redirect_to "/"
+		else
+			render 'new'
+		end
+	end
+
+	private
+		def student_params
+			params.require(:student).permit(:name, :email)
+		end
 end
